898b6dfee4706d009b4c06cb42f3321a99bbdf23
devili4enko <devili4enko@gmail.com>
2014-09-27 11:29:32 +0300

    Singleton and Threadsafe Singleton

---
+++ PatternsProject/Singleton/HardProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class HardProcessor
+    {
+        private int start;
+
+        public HardProcessor(int start)
+        {
+            this.start = start;
+            LoggerSingleton.GetInstance().Log("Process Loaded");
+        }
+
+        public void Process(int end)
+        {
+            LoggerSingleton.GetInstance().Log("Process Started");
+            int sum = 0;
+            for (; start <= end; start++)
+            {
+                sum++;
+            }
+            LoggerSingleton.GetInstance().Log("Process Ended. Result: " + sum);
+        }
+    }
+}
---
+++ PatternsProject/Singleton/SecondProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class SecondProcessor
+    {
+         private int start;
+
+         public SecondProcessor(int start)
+        {
+            this.start = start;
+            ThreadSafeSingletonLogger.GetInstance().Log("Process Loaded");
+        }
+
+        public void Process(int end)
+        {
+            ThreadSafeSingletonLogger.GetInstance().Log("Process Started");
+            int sum = 0;
+            for (; start <= end; start++)
+            {
+                sum++;
+            }
+            ThreadSafeSingletonLogger.GetInstance().Log("Process Ended. Result: " + sum);
+        }
+    }
+}
