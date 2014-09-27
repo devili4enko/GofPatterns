9863a119e40d31bebdffb9f53e920f68049010f8
devili4enko <devili4enko@gmail.com>
2014-09-27 11:40:21 +0300

    Command

---
+++ PatternsProject/Command/MyCommandProject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command
+{
+    class MyCommandProject : ICommand
+    {
+        protected Team _team { get; set; }
+        protected List<Requirement> _requirements { get; set; }
+
+        public MyCommandProject(Team team, List<Requirement> requirements )
+        {
+            _team = team;
+            _requirements = requirements;
+        }
+
+        public void Execute()
+        {
+            _team.CompleteProject(_requirements);
+        }
+    }
+}
---
+++ PatternsProject/Command/Requirement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Command
+{
+    class Requirement
+    {
+        public string Task;
+
+        public Requirement(string task)
+        {
+            Task = task;
+        }
+    }
+}
