293a190f772f421826e8cd5e8a389ac06c7a7f8c
devili4enko <devili4enko@gmail.com>
2014-09-27 11:34:21 +0300

    Facade

---
+++ PatternsProject/Facade/RentRoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facade
+{
+    class RentRoom
+    {
+        public int Rent(int roomQuality)
+        {
+            switch (roomQuality)
+            {
+                case 3:
+                    return 200;
+                case 4:
+                    return 300;
+                case 5:
+                    return 400;
+                default:
+                    throw new ArgumentException("Please choose from 3 to 5");
+            }
+        }
+    }
+}
---
+++ PatternsProject/Facade/RentSki.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facade
+{
+    class RentSki
+    {
+        public int RentBoots(int feetSize, int skierLevel)
+        {
+            return 20;
+        }
+
+        public int RentSkies(int skierHeight, int skierWieght)
+        {
+            return 40;
+        }
+
+        public int RentCloses(int skierSize)
+        {
+            return 60;
+        }
+    }
+}
