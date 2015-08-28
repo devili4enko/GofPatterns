8ac5842ec5a37db7b03bdeb58b08d48af9b167cf
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2015-08-28 10:59:14 +0300

    Add ThreadSafeSingleton

---
+++ PatternsProject/Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Singleton
+{
+    public class ThreadSafeSingleton
+    {
+        private ThreadSafeSingleton() { }
+        private int count = 0;
+        public static ThreadSafeSingleton instanceLoggerSingleton = null;
+
+        private static object _lock = new object();
+
+        public static ThreadSafeSingleton GetInstance()
+        {
+            if (instanceLoggerSingleton != null) return instanceLoggerSingleton;
+
+            var temp = new ThreadSafeSingleton();
+            Interlocked.CompareExchange(ref instanceLoggerSingleton, temp, null);
+
+            return instanceLoggerSingleton;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("{0} logging: {1}", count, message);
+            count++;
+        }
+    }
+}
