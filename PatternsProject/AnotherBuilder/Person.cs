d16aa9cb4002b2b886130995f9d07deee1bfe456
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2015-08-28 10:39:27 +0300

    Add impl

---
+++ PatternsProject/AnotherBuilder/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherBuilder
+{
+   public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string SurName { get; set; }
+
+        public Hobbie Hobbie { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Hi I'm {0} and I {0} years old", Name, Age);
+        }
+
+    }
+}
---
+++ PatternsProject/AnotherBuilder/PersonBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherBuilder
+{
+    class PersonBuilder : AbstractBuilder<Person>
+    {
+        private Person _person;
+
+        public PersonBuilder()
+        {
+            _person = new Person();
+        }
+
+        public PersonBuilder WithAge(int age)
+        {
+            _person.Age = age;
+            return this;
+        }
+
+        public PersonBuilder WithName(string name)
+        {
+            _person.Name = name;
+            return this;
+        }
+
+
+        public Person Build()
+        {
+            return this._person;
+        }
+
+        public Person Build(Person instance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
