0bf4f97f436712b1ef3d0f361261781778c3b27f
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2015-08-28 10:38:59 +0300

    Add Another Builder project

---
+++ PatternsProject/AnotherBuilder/AbstractBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherBuilder
+{
+    interface AbstractBuilder<T> where T: class 
+    {
+        T Build();
+
+        T Build(T instance);
+    }
+}
---
+++ PatternsProject/AnotherBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presonBuilder = new PersonBuilder();
+
+            var person = presonBuilder.WithAge(18).WithName("Alex").Build();
+
+            Console.WriteLine(person);
+        }
+    }
+}
